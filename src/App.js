4dcd91f2065da87bb4183caad91d06ba07eee3ee
Rodolpho <redsmedeiros@gmail.com>
2022-11-13 18:40:58 -0300

    estilizacao home

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,25 @@ function App() {
         <a href='/'>Ecommerce</a>
       </header>
       <main>
-        List Products
+        <h1>Products</h1>
+        <div className="products">
+        {
+          //codigo js: chamar data e acessar produtos com a função map que vai iterar cada obj - cada obj será um jsx
+          data.products.map( product => (
+            <div className="product" key={product.slug}>
+              <a href={`/products/${product.slug}`}>
+                <img src={product.image} alt={product.name}/>
+              </a>
+              
+              <div className="product-info">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))
+        }
+        </div>
       </main>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,30 @@
+const data = {
+    products:[
+        {
+            name: 'Nike Slim',
+            slug: 'nike-slim',
+            category: 'Shirts',
+            image: '/images/1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            descriptions: 'hight-quality'
+        },
+        {
+            name: 'Adidas Slim',
+            slug: 'adidas-slim',
+            category: 'Shirts',
+            image: '/images/2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            descriptions: 'hight-quality'
+        },
+    ]
+}
+
+export default data
