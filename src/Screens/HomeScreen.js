15360604a5ea7767302de5fe1703511238096568
Rodolpho <redsmedeiros@gmail.com>
2022-11-22 15:43:57 -0300

    Criacao do usereducer

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,13 +1,57 @@
-import data from "../data"
+//import data from "../data"
 import {Link} from 'react-router-dom'
+import { useEffect, useReducer } from 'react'
+import axios from 'axios'
+
+const reducer = (state, action) =>{
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return {...state, loading: true}
+    case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading: false}
+    case 'FETCH_FAIL':
+      return {...state, loading: false, error: action.payload}
+    default:
+      return state
+  }
+}
 
 const HomeScreen = ()=>{
+  
+  const [{loading, error, products}, dispatch] = useReducer(reducer,{loading: true, error:'', products: []})
+
+  useEffect(()=>{
+
+    const fetchData = async ()=>{
+
+      dispatch({type: 'FETCH_REQUEST'})
+
+      try{
+        
+        const result = await axios.get('api/products')
+
+        dispatch({type: 'FETCH_SUCCESS', payload: result.data})
+
+      }catch(err){
+
+        dispatch({type: 'FETCH_FAIL', payload: err.message})
+
+      }
+     
+    }
+
+    fetchData()
+
+  }, [])
+
     return (
         <div><h1>Products</h1>
         <div className="products">
         {
-          //codigo js: chamar data e acessar produtos com a função map que vai iterar cada obj - cada obj será um jsx
-          data.products.map( product => (
+          loading ? (<div>Loading...</div>) :
+          error ? (<div>{error}</div>) :
+          (
+          products.map( product => (
             <div className="product" key={product.slug}>
               <Link to={`/products/${product.slug}`}>
                 <img src={product.image} alt={product.name}/>
@@ -22,7 +66,7 @@ const HomeScreen = ()=>{
                 <button>Add to cart</button>
               </div>
             </div>
-          ))
+          )))
         }
         </div></div>
     )
